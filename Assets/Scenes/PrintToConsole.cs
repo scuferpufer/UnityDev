1d041fc9ed9d82f8493acdabc9d147dfb9e2b8d5
ScuffedDev <michael.amaya720@gmail.com>
2024-05-16 21:57:09 -0700

    Initial commit

---
+++ Assets/Scenes/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("This is super smash bros huh?");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
